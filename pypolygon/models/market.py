cffbb79795e1cbc18cc08ec8e235e2563e7a1947
Simone Sturniolo <simonesturniolo@gmail.com>
2024-02-14 18:25:06 +0000

    Refactor TickerResult model and add aggregation method

--- pypolygon/models/market.py
+++ pypolygon/models/market.py
@@ -1,17 +1,55 @@
 from typing import Optional, List
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 class TickerResult(BaseModel):
-    c: float
-    h: float
-    l: float  # noqa: E741
-    n: int
-    o: float
-    t: int
-    v: int
-    vw: float
-    ticker: Optional[str] = None
+    c: float = Field(..., description="Close price")
+    h: float = Field(..., description="Highest price")
+    l: float = Field(..., description="Lowest price")  # noqa: E741
+    n: int = Field(..., description="Number of transactions")
+    o: float = Field(..., description="Open price")
+    t: int = Field(..., description="Unix Msec Timestamp")
+    v: int = Field(..., description="Trading volume")
+    vw: float = Field(..., description="Volume weighted average price")
+    otc: Optional[bool] = Field(None, description="Over the counter")
+    ticker: Optional[str] = Field(None, description="Ticker name")
+    
+    
+    @classmethod
+    def aggregate(cls, data: List["TickerResult"]) -> "TickerResult":
+        # Sort by timestamp
+        data = sorted(data, key=lambda x: x.t)
+        
+        otc_0 = data[0].otc
+        ticker_0 = data[0].ticker
+        
+        if not all([x.otc == otc_0 for x in data]):
+            raise ValueError("OTC mismatch")
 
+        if not all([x.ticker == ticker_0 for x in data]):
+            raise ValueError("Ticker mismatch")
+        
+        o_tot = data[0].o
+        c_tot = data[-1].c
+        n_tot = sum([x.n for x in data])
+        v_tot = sum([x.v for x in data])
+        h_tot = max([x.h for x in data])
+        l_tot = min([x.l for x in data])
+        t_tot = data[0].t
+        vw_tot = sum([x.vw*x.v for x in data]) / v_tot
+        
+        return cls(
+            o=o_tot,
+            c=c_tot,
+            h=h_tot,
+            l=l_tot,
+            n=n_tot,
+            t=t_tot,
+            v=v_tot,
+            vw=vw_tot,
+            otc=otc_0,
+            ticker=ticker_0
+        )
+        
 class AggregatesResponse(BaseModel):
     adjusted: bool
     queryCount: int
---
+++ tests/test_models.py
@@ -0,0 +1,51 @@
+import pytest
+from pypolygon.models.market import TickerResult
+
+
+def test_tickers():
+    
+    t1 = TickerResult(
+        c=1,
+        h=2,
+        l=0,
+        n=100,
+        o=0.5,
+        t=1000,
+        v=1000,
+        vw=1,
+        otc=True,
+        ticker="AAPL"
+    )
+    
+    t2 = TickerResult(
+        c=2,
+        h=3,
+        l=1,
+        n=100,
+        o=1,
+        t=2000,
+        v=1000,
+        vw=2,
+        otc=True,
+        ticker="AAPL"
+    )
+    
+    tsum = TickerResult.aggregate([t1, t2])
+    
+    assert tsum.c == 2
+    assert tsum.h == 3
+    assert tsum.l == 0
+    assert tsum.n == 200
+    assert tsum.o == 0.5
+    assert tsum.t == 1000
+    assert tsum.v == 2000
+    assert tsum.vw == 1.5
+    assert tsum.otc
+    assert tsum.ticker == "AAPL"
+    
+    # Try with different tickers
+    t2.ticker = "TSLA"
+    
+    with pytest.raises(ValueError):
+        TickerResult.aggregate([t1, t2])
+        
